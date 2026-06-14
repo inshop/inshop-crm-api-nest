327f29585bffbd0fa69be5149559d71161f554bf
Maksym Mukovoz <sgmaxim@gmail.com>
2026-06-14 18:03:17 +0200

    Upgrade packages version

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { App } from 'supertest/types';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
